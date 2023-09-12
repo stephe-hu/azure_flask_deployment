b6e89f67bfdca61bce30e77aa515e5035d90d74b
stephe-hu <stephen.hu@stonybrook.edu>
2023-09-12 01:42:29 +0000

    changes

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template
+import random
+from faker import Faker
 import pandas as pd
+
+fake = Faker()
+
+# small change
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def mainpage():
     return render_template('base.html')
+
+@app.route('/about')
+def aboutpage():
+    return render_template('about.html')
+
+@app.route('/random')
+def randomnumber():
+    number_var = random.randint(1, 100000)
+    fake_address = fake.address()
+    return render_template('random.html', single_number = number_var, single_address = fake_address)
 
 if __name__ == '__main__':
     app.run(
